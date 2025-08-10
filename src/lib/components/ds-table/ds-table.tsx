591b3e00bceea198babdddef9d19a1b0a6375400
Avraham Hamu 🏂 <ah584d@intl.att.com>
2025-08-10 16:56:18 +0300

    add pagination

--- src/lib/components/ds-table/ds-table.tsx
+++ src/lib/components/ds-table/ds-table.tsx
@@ -126,6 +126,7 @@ const DsTable = <TData extends { id: string }, TValue>({
 	useImperativeHandle(
 		ref,
 		() => ({
+			// Selection API
 			selectRow: (rowId: string) => {
 				table.getRow(rowId).toggleSelected(true);
 			},
@@ -146,6 +147,13 @@ const DsTable = <TData extends { id: string }, TValue>({
 			getSelectedRows: () => {
 				return table.getFilteredSelectedRowModel().rows.map((r) => r.original);
 			},
+			// Pagination API
+			nextPage: () => table.nextPage(),
+			previousPage: () => table.previousPage(),
+			getPageCount: () => table.getPageCount(),
+			getCanNextPage: () => table.getCanNextPage(),
+			getCanPreviousPage: () => table.getCanPreviousPage(),
+			getState: () => table.getState(),
 		}),
 		[table],
 	);

74add1ace746d2dac35d94baf5e5b1d55a5c56db
vpolessky <valentine.polessky@drivenets.com>
2026-03-19 13:33:07 +0100

    chore(design-system): remove unused file [AR-53392]

--- packages/design-system/src/components/ds-table/stories/common/story-test-helpers.ts
+++
@@ -1,4 +0,0 @@
-import type { StoryContext } from '@storybook/react-vite';
-
-export const getDataRows = (canvas: StoryContext['canvas']) =>
-	canvas.getAllByRole('row').filter((row: HTMLElement) => !row.querySelector('th'));
